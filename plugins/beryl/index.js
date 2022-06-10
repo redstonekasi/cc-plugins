b6d547648ea4fef9767305fba4b4d8735b11d603
redstonekasi <kasi@hanneskann.de>
2022-06-10 19:09:31 +0200

    [beryl] replace beryl with cumcord settings on unload

--- plugins/beryl/index.js
+++ plugins/beryl/index.js
@@ -1,7 +1,10 @@
 import { findByDisplayName } from "@cumcord/modules/webpack";
 import { after, findAndPatch } from "@cumcord/patcher";
+import { ReactDOM } from "@cumcord/modules/common";
 import Beryl from "./components/Beryl";
 import injectStyles from "./styles.css";
+
+let originalSettings;
 
 const uninjectStyles = injectStyles();
 
@@ -10,7 +13,10 @@ const unpatch = findAndPatch(
   (SettingsView) =>
     after("getPredicateSections", SettingsView.prototype, (args, items) => {
       const index = items.findIndex((val) => val.section === "CUMCORD_PLUGINS");
-      if (index > 0) items[index].element = Beryl;
+      if (index > 0) {
+        originalSettings = items[index].element;
+        items[index].element = Beryl;
+      }
       return items;
     }),
 );
@@ -18,4 +24,8 @@ const unpatch = findAndPatch(
 export const onUnload = () => {
   uninjectStyles();
   unpatch();
+
+  // There has to be a better way to do this
+  const view = document.getElementById("cumcord_plugins-tab");
+  if (view) ReactDOM.render(React.createElement(originalSettings), view);
 };
