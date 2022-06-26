9e9b51ccd887240048d7eacafe10757893b10c85
redstonekasi <kasi@hanneskann.de>
2022-06-27 01:26:05 +0200

    [beryl] fix uninjecting after new settings api update

--- plugins/beryl/index.js
+++ plugins/beryl/index.js
@@ -3,19 +3,36 @@ import { after, findAndPatch } from "@cumcord/patcher";
 import Beryl from "./components/Beryl";
 import injectStyles from "./styles.css";
 
+let ref;
+let CumcordSettings;
+
 const uninjectStyles = injectStyles();
 
 const unpatch = findAndPatch(
   () => findByDisplayName("SettingsView"),
   (SettingsView) =>
-    after("getPredicateSections", SettingsView.prototype, (args, items) => {
-      const index = items.findIndex((val) => val.section === "CUMCORD_PLUGINS");
-      if (index > 0) items[index].element = Beryl;
-      return items;
-    }),
+    after(
+      "getPredicateSections",
+      SettingsView.prototype,
+      (_, items) => {
+        const index = items.findIndex((val) => val.section === "CUMCORD_PLUGINS");
+        if (index > 0) {
+          if (ref === undefined || CumcordSettings === undefined) {
+            ref = items[index];
+            CumcordSettings = items[index].element;
+          }
+
+          items[index].element = Beryl;
+        }
+        return items;
+      },
+      true,
+    ),
 );
 
 export const onUnload = () => {
   uninjectStyles();
   unpatch();
+
+  if (ref !== undefined && CumcordSettings !== undefined) ref.element = CumcordSettings;
 };
