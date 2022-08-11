fab80252d76ab1af2935014cd308880fcc42740d
redstonekasi <kasi@hanneskann.de>
2022-08-11 21:51:35 +0200

    [picture-links] fix nitro banners

--- plugins/picture-links/patches/banners.js
+++ plugins/picture-links/patches/banners.js
@@ -2,22 +2,20 @@ import { findByDisplayName, findByProps } from "@cumcord/modules/webpack";
 import { after, findAndPatch } from "@cumcord/patcher";
 import { findInReactTree } from "@cumcord/utils";
 
-const Banners = findByProps("getUserBannerURL");
-
 export default () =>
   findAndPatch(
     () => findByDisplayName("UserBanner", false),
     (UserBanner) =>
-      after("default", UserBanner, ([{ user }], res) => {
+      after("default", UserBanner, ([{ user, displayProfile }], res) => {
         const handler = findInReactTree(res.props.children, (p) => p?.onClick);
         const image =
           user.banner === "_"
             ? res.props.style.backgroundImage?.match?.(/^url\((.*)\)$/)?.[1]
-            : Banners.getUserBannerURL({
-                ...user,
-                canAnimate: true,
-              })
-                ?.replace(/(?:\?size=\d{3,4})?$/, "?size=4096")
+            : displayProfile
+                ?.getBannerURL({
+                  size: 4096,
+                  canAnimate: true,
+                })
                 ?.replace(".webp", ".png");
 
         if (!handler?.children && image) {
